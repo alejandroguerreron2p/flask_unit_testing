79c114ee2601454088f0acd513eb99ef052b69db
Alejandro Guerrero <alejandro.guerrero@net2phone.com>
2024-01-11 00:41:05 -0600

    removed business logic

--- business_logic.py
+++
@@ -1,9 +0,0 @@
-from database import retrieve_book_db, create_book_db
-
-def retrieve_book_logic(book_id):
-    # Business logic can be added here if needed
-    return retrieve_book_db(book_id)
-
-def create_book_logic(data):
-    # Business logic can be added here if needed
-    return create_book_db(data)

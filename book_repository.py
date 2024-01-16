75f6be360c32c3dbdf42cf4dd0c06a666edae4a7
Alejandro Guerrero <alejandro.guerrero@net2phone.com>
2024-01-15 19:40:51 -0600

    added delete and update logic, changed id datatype and created a "real" repository

--- book_repository.py
+++ book_repository.py
@@ -1,4 +1,4 @@
-from api_dict import test_data
+import uuid
 
 class BookRepositoryInterface:
     def find_book_by_id(self, book_id):
@@ -15,24 +15,59 @@ class HashBookRepository(BookRepositoryInterface):
         self.data = data
         
     def find_book_by_id(self, book_id):
-        if int(book_id) in self.data:
-            return self.data[int(book_id)]
+        if book_id in self.data:
+            return self.data[book_id]
         else:
             return {'error': 'Book not found'}
                 
     def create_book(self, data):
         # Insert into Test Data dict
-        self.data[len(self.data)+1] = data
-
+        new_id = str(uuid.uuid4())
+        self.data[new_id] = data
         # Return the newly created book's ID
-        return {'message': 'Book created successfully', 'book_id': str(len(self.data))}
+        return {'message': 'Book created successfully', 'book_id': new_id}
     
     def update_book(self, book_id, data):
-        self.data[book_id] = data
-        return {'message': 'Book updated successfully', 'book_id': str(book_id)}
+        if book_id in self.data:
+            self.data.update({book_id: data})
+            return {'message': 'Book updated successfully', 'book_id': book_id}
+        else:
+            return {'error': 'Book not found to update', 'book_id': book_id}
     
     def delete_book(self, book_id):
-        self.data.pop(book_id)
-        return {'message': 'Book deleted successfully', 'book_id': str(book_id)}
+        if book_id in self.data:
+            self.data.pop(book_id)
+            return {'message': 'Book deleted successfully', 'book_id': book_id}
+        else:
+            return {'error': 'Book not found to delete', 'book_id': book_id}
         
+class RealBookRepository(BookRepositoryInterface):
+    def __init__(self, data: dict):
+        self.data = data
         
+    def find_book_by_id(self, book_id):
+        if book_id in self.data:
+            return self.data[book_id]
+        else:
+            return {'error': 'Book not found'}
+                
+    def create_book(self, data):
+        # Insert into Test Data dict
+        new_id = str(uuid.uuid4())
+        self.data[new_id] = data
+        # Return the newly created book's ID
+        return {'message': 'Book created successfully', 'book_id': new_id}
+    
+    def update_book(self, book_id, data):
+        if book_id in self.data:
+            self.data.update({book_id: data})
+            return {'message': 'Book updated successfully', 'book_id': book_id}
+        else:
+            return {'error': 'Book not found to update', 'book_id': book_id}
+    
+    def delete_book(self, book_id):
+        if book_id in self.data:
+            self.data.pop(book_id)
+            return {'message': 'Book deleted successfully', 'book_id': book_id}
+        else:
+            return {'error': 'Book not found to delete', 'book_id': book_id}

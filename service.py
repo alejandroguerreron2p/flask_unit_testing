09d1c477b04730e255157d7cf1951c76950287ea
Alejandro Guerrero <alejandro.guerrero@net2phone.com>
2024-01-15 19:42:00 -0600

    added update and delete logic

--- service.py
+++ service.py
@@ -1,7 +1,5 @@
-from flask import jsonify
 from marshmallow import ValidationError
 from book import Book
-from book_repository import BookRepositoryInterface
 
 class ServiceInterface():
     def retrieve_book(self, book_id):
@@ -19,18 +17,27 @@ class Service():
         
     def retrieve_book(self, book_id):
         result = self.book_repo.find_book_by_id(book_id)
-        if "error" not in result:
-            return result
-        else:
-            return jsonify(result), 400
+        return result
 
     def create_book(self, data):
         schema = Book()
         try:
-            validation = schema.load(data)
-            if validation:
-                result = self.book_repo.create_book(data) 
-                return result
+            schema.load(data)
+            result = self.book_repo.create_book(data) 
+            return result
         except ValidationError as err:
-            return jsonify(err.messages), 400
+            return {"error": err.messages}
+        
+    def update_book(self, book_id, data):
+        schema = Book() 
+        try:
+            schema.load(data)
+            result = self.book_repo.update_book(book_id, data)
+            return result
+        except ValidationError as err:
+            return {"error": err.messages}
+        
+    def delete_book(self, book_id):
+        result = self.book_repo.delete_book(book_id)
+        return result
     
